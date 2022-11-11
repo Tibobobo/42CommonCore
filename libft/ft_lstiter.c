4a95f15d652565cec4ec925e0b89dd138f018038
Thibaut Grasset <tgrasset@e2r5p16.clusters.42paris.fr>
2022-11-11 13:38:56 +0100

    lstiter added

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,8 +6,17 @@
 /*   By: tgrasset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 18:52:36 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/11/10 18:52:39 by tgrasset         ###   ########.fr       */
+/*   Updated: 2022/11/11 11:02:59 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst != NULL)
+	{
+		(*f)(lst->content);
+		lst = lst->next;
+	}
+}
