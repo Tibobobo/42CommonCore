2ca76a3a958395f60a1257b30a8cdf25be2ada2e
Thibaut Grasset <tgrasset@e1r8p9.clusters.42paris.fr>
2022-11-14 17:37:40 +0100

    calloc secured

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tgrasset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:31:31 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/11/14 16:02:04 by tgrasset         ###   ########.fr       */
+/*   Updated: 2022/11/14 17:37:23 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
 
+	if (nmemb > 0 && (size_t) - 1 / nmemb < size)
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (!p)
 		return (NULL);
