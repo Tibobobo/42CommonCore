81ae17062455c6923c5a70708fc9d7519f649307
Thibaut Grasset <tgrasset@e1r8p9.clusters.42paris.fr>
2022-11-14 18:03:01 +0100

    GNL started

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tgrasset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:46:15 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/11/14 17:47:07 by tgrasset         ###   ########.fr       */
+/*   Updated: 2022/11/14 18:01:08 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 char	*get_next_line(int fd)
 {
-	
+	char	*line;
+	char	buffer[BUFFER_SIZE];
+	char	*rest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	read(fd, buffer, BUFFER_SIZE);
 }
