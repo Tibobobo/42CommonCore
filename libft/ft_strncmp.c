85bcfdadb02bed5fa583e8280747aa073d30167d
Thibaut Grasset <tgrasset@e2r11p13.clusters.42paris.fr>
2022-11-15 09:49:20 +0100

    fixed strncmp by casting in unsigned char the result of the substraction

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tgrasset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:14:19 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/11/07 17:53:43 by tgrasset         ###   ########.fr       */
+/*   Updated: 2022/11/15 09:44:02 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i == n)
 		return (0);
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
