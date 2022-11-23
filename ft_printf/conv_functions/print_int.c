c00e6c1f1078b13d810fe3f61cb078d26b0d9418
Thibaut GRASSET <tgrasset@e1r8p18.clusters.42paris.fr>
2022-11-23 10:06:03 +0100

    added putnbr base

--- ft_printf/conv_functions/print_int.c
+++ ft_printf/conv_functions/print_int.c
@@ -6,7 +6,7 @@
 /*   By: tgrasset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 11:47:29 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/11/22 11:47:53 by tgrasset         ###   ########.fr       */
+/*   Updated: 2022/11/23 09:48:06 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int	print_int(va_list args)
 {
-	
+    char    *str;
+
+    str = ft_itoa(va_arg, int);
+    ft_putstr_fd(str, 1);
+	free(str);
+    return (ft_strlen(str));
 }
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_dec.c                                        :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tgrasset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/22 11:46:55 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/11/22 11:47:20 by tgrasset         ###   ########.fr       */
+/*   Created: 2022/11/23 09:33:25 by tgrasset          #+#    #+#             */
+/*   Updated: 2022/11/23 10:04:58 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	print_dec(va_list args)
+void	putnbr_base(int	n, char *base)
 {
-    
+	long int	nb;
+	int			base_len;
+
+	nb = n;
+	base_len = ft_strlen(base);
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', 1);
+		nb = -nb;
+	}
+	if (nb >= base_len)
+	{
+		ft_putnbr_base(nb / base_len);
+		ft_putnbr_base(nb % base_len, base)
+	}
+	else
+		ft_putchar_fd(base[nb], 1);
 }
