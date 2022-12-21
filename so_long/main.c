e312928dcde08b5eedd5d0f76c9b0e66c151e9cf
Thibaut GRASSET <tgrasset@e3r9p20.clusters.42paris.fr>
2022-12-21 13:33:42 +0100

    main simplified

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: tgrasset <tgrasset@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 18:06:53 by tgrasset          #+#    #+#             */
-/*   Updated: 2022/12/21 12:20:00 by tgrasset         ###   ########.fr       */
+/*   Updated: 2022/12/21 13:32:51 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,28 @@
 // TO_DO : Makefile relinks mlx
 // 		Add enemies to allowed chars
 
+static t_map	full_check(int ac, char **av, t_map *map)
+{
+	map_pre_check(ac, av);
+	map->grid = parse_map(av[1]);
+	if (map->grid == NULL)
+		ft_error(3);
+	if (is_valid_map(map) == 0)
+	{
+		free_map(map->grid);
+		ft_error(4);
+	}
+	solve(*map, av[1], map->px, map->py);
+	return (*map);
+}
+
 int	main(int ac, char **av)
 {
 	t_map	map;
 	int		i;
 
 	i = 0;
-	map_pre_check(ac, av);
-	map.grid = parse_map(av[1]);
-	if (map.grid == NULL)
-		ft_error(3);
-	if (is_valid_map(&map) == 0)
-	{
-		free_map(map.grid);
-		ft_error(4);
-	}
-	solve(map, av[1], map.px, map.py);
+	map = full_check(ac, av, &map);
 	while (map.grid[i] != NULL)
 	{
 		ft_printf("%s\n", map.grid[i]);
