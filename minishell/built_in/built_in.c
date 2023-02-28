9e53617261192f422f6bccf0e991b8b075a1eb6e
Thibaut GRASSET <tgrasset@e1r8p7.clusters.42paris.fr>
2023-02-28 16:38:42 +0100

    norminette done

--- minishell/built_in/built_in.c
+++ minishell/built_in/built_in.c
@@ -6,7 +6,7 @@
 /*   By: tgrasset <tgrasset@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 10:39:32 by tgrasset          #+#    #+#             */
-/*   Updated: 2023/02/28 16:34:34 by tgrasset         ###   ########.fr       */
+/*   Updated: 2023/02/28 16:37:43 by tgrasset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ void	check_built_in(t_sh *sh, t_comm *cmd, char **env, int forked)
 	else if (ft_strncmp(cmd->file, "pwd", 4) == 0)
 		my_pwd(sh);
 	else if (ft_strncmp(cmd->file, "export", 7) == 0)
-	    my_export(sh, cmd, env, forked);
+		my_export(sh, cmd, env, forked);
 	else if (ft_strncmp(cmd->file, "unset", 6) == 0)
-	    my_unset(sh, cmd, forked);
+		my_unset(sh, cmd, forked);
 	else if (ft_strncmp(cmd->file, "env", 4) == 0)
-	    my_env(sh, cmd, env);
+		my_env(sh, cmd, env);
 	else if (ft_strncmp(cmd->file, "exit", 5) == 0)
 		my_exit(sh, cmd, forked);
 }
