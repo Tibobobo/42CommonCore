36af6c9891ab69e445d01cfeb35c0c14b4bf0482
Tibo <thibautgrasset@MacBook-Pro-de-Thibaut.local>
2023-05-09 21:30:23 +0200

    day 8 done

--- CPP/08/ex02/MutantStack.hpp
+++ CPP/08/ex02/MutantStack.hpp
@@ -1,17 +1,35 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# include <stack>
+# include <algorithm>
+
 template <typename T>
 class MutantStack : public std::stack<T> {
 
 	public:
 
-		MutantStack(void) {}
-		MutantStack(MutantStack const &src) {}
+		MutantStack(void) : std::stack<T>() {};
+		MutantStack(MutantStack const &src) : std::stack<T>(src) {};
 
-		~MutantStack(void) {}
+		virtual ~MutantStack(void) {};
 
-		MutantStack &operator=(MutantStack const &rhs) {}
+		MutantStack &operator=(MutantStack const &rhs) {
+			if (this == &rhs)
+				return (*this);
+			std::stack<T>::operator=(rhs);
+			return (*this);
+		};
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin(void) {
+			return (std::stack<T>::c.begin());
+		}
+
+		iterator	end(void) {
+			return (std::stack<T>::c.end());
+		}
 	
 	private:
 
--- CPP/08/ex02/main.cpp
+++ CPP/08/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
 int main() {
 
@@ -31,5 +32,35 @@ int main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << std::endl << "----------------------------------------------------------" << std::endl << std::endl;
+
+	std::list<int> lst;
+
+	lst.push_back(5);
+	lst.push_back(17);
+
+	std::cout << lst.back() << std::endl;
+
+	lst.pop_back();
+
+	std::cout << lst.size() << std::endl;
+
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	//[...]
+	lst.push_back(0);
+
+	std::list<int>::iterator it2 = lst.begin();
+	std::list<int>::iterator ite2 = lst.end();
+
+	++it2;
+	--it2;
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
 	return 0;
 }
