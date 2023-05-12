c5053f9df664b41d5ffe009ada1ddb6c3f69cc2f
Tibo <thibautgrasset@MacBook-Pro-de-Thibaut.local>
2023-05-12 20:25:01 +0200

    ex 01 done

---
+++ CPP/09/ex01/RPN.cpp
@@ -0,0 +1,97 @@
+# include "RPN.hpp"
+
+RPN::RPN(std::string str) : _str(str) {
+
+}
+
+RPN::~RPN(void) {
+
+}
+
+void RPN::calculate(void) {
+
+    if (_str.length() == 1 && std::isdigit(*_str.begin()))
+    {
+        std::cout << _str << std::endl;
+        exit (0);
+    }
+    if (checkFormat(_str) == false)
+    {
+        std::cerr << "Error" << std::endl;
+		exit (-1);
+    }
+    int a = 0;
+    int b = 0;
+    for (std::string::iterator it = _str.begin(); it != _str.end(); it++)
+    {
+        if (std::isdigit(*it))
+            _numbers.push(*it - '0');
+        else if (isOperator(*it) == true)
+        {
+            if (_numbers.size() < 2)
+            {
+                std::cerr << "Error" << std::endl;
+		        exit (-1);
+            }
+            a = _numbers.top();
+            _numbers.pop();
+            b = _numbers.top();
+            _numbers.pop();
+            _numbers.push(operation(a, b, *it));
+        }
+    }
+    std::cout << _numbers.top() << std::endl;
+}
+
+bool	RPN::isOperator(char c) const {
+
+	if (c == '+' || c == '-' || c == '*' || c == '/')
+		return (true);
+	return (false);
+}
+
+bool	RPN::checkFormat(std::string str) const {
+
+	int num = 0;
+	int op = 0;
+	if (!std::isdigit(str[0]) || !isOperator(str[str.length() - 1]))
+		return (false);
+	for (std::string::iterator it = str.begin(); it != str.end(); it++)
+	{
+		if (*it == ' ' && ((it + 1) != str.end() && *(it + 1) != ' '))
+			;
+		else if (std::isdigit(*it) && *(it + 1) == ' ')
+			num++;
+		else if (isOperator(*it))
+		{
+			op++;
+			if (it + 1 != str.end() && *(it + 1) != ' ')
+				return (false);
+		}
+		else
+			return (false);
+	}
+	if (num - op != 1)
+		return (false);
+	return (true);
+}
+
+int		RPN::operation(int a, int b, char op) const {
+
+    if (op == '+')
+        return (b + a);
+    else if (op == '-')
+        return (b - a);
+    else if (op == '*')
+        return (b * a);
+    else
+    {
+        if (a == 0)
+        {
+            std::cerr << "[1]     4242 floating point exception   ./RPN \"" << _str << "\"";
+            std::cerr << "Naaaaaah just kidding =)" << std::endl;
+            exit (-1);
+        }
+        return (b / a);
+    }
+}
--- CPP/09/ex01/RPN.hpp
+++ CPP/09/ex01/RPN.hpp
@@ -11,12 +11,18 @@ class RPN {
 		RPN(std::string str);
 		~RPN(void);
 
+		void calculate(void);
+		bool	checkFormat(std::string str) const;
+		bool	isOperator(char c) const;
+		int		operation(int a, int b, char op) const;
+
 	private:
 
 		RPN(void);
 		RPN(RPN const &src);
 		RPN &operator=(RPN const &rhs);
 		std::stack<int> _numbers;
+		std::string _str;
 };
 
 #endif
