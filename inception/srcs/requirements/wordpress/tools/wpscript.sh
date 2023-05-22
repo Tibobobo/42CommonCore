acf69c4be95ea3e64690a7b1616a4a700447cd50
you@example.com <you@example.com>
2023-05-22 17:53:14 +0200

    lol

--- inception/srcs/requirements/wordpress/tools/wpscript.sh
+++ inception/srcs/requirements/wordpress/tools/wpscript.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#set -eux
 
-cd /var/www/wordpress
+sleep 10;
+cd /var/www/html/wordpress
 
-if ! wp core is-installed; then
-wp config create	--allow-root --dbname=${SQL_DATABASE} \
-			--dbuser=${SQL_USER} \
-			--dbpass=${SQL_PASSWORD} \
-			--dbhost=${SQL_HOST} \
-			--url=https://${DOMAIN_NAME};
+if [ ! -d /var/www/html/wordpress/wp-config.php ]; then
+wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/html/wordpress'
 
 wp core install	--allow-root \
 			--url=https://${DOMAIN_NAME} \
@@ -24,19 +24,6 @@ wp user create		--allow-root \
 
 wp cache flush --allow-root
 
-# it provides an easy-to-use interface for creating custom contact forms and managing submissions, as well as supporting various anti-spam techniques
-wp plugin install contact-form-7 --activate
-
-# set the site language to English
-wp language core install en_US --activate
-
-# remove default themes and plugins
-wp theme delete twentynineteen twentytwenty
-wp plugin delete hello
-
-# set the permalink structure
-wp rewrite structure '/%postname%/'
-
 fi
 
 if [ ! -d /run/php ]; then
@@ -44,4 +31,4 @@ if [ ! -d /run/php ]; then
 fi
 
 # start the PHP FastCGI Process Manager (FPM) for PHP version 7.3 in the foreground
-exec /usr/sbin/php-fpm7.3 -F -R
+exec /usr/sbin/php-fpm7.3 -F
