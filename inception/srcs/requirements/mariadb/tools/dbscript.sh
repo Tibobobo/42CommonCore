196531c0a16540b68b683b981809a3fe6842e185
moi <toto@example.com>
2023-09-13 17:54:00 +0200

    plop

--- inception/srcs/requirements/mariadb/tools/dbscript.sh
+++ inception/srcs/requirements/mariadb/tools/dbscript.sh
@@ -3,13 +3,21 @@
 
 service mysql start;
 
-# log into MariaDB as root and create database and the user
+#creation de la database en recuperant son nom dans la variable d'environnement correspondante
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+#creation du user
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+#donner les droits au user sur cette database
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+#modification du user root
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+#rafraichissement pour mettre a jour ces changements
 mysql -e "FLUSH PRIVILEGES;"
 
+#on redemarre mysql pour que les changements soient pris en compte
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-#mysqladmin -u root shutdown
 exec mysqld_safe
