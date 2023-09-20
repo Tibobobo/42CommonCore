a81b1a636a7876ba31a63802f14e75b6ad68fd26
moi <toto@example.com>
2023-09-20 11:43:34 +0200

    done

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -11,23 +11,25 @@ else
 	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"  >> /var/lib/mysql/config_db.sql
 	echo "FLUSH PRIVILEGES;" >> /var/lib/mysql/config_db.sql
 
-	echo "---> Starting MYSQL"
+	echo "Starting MYSQL"
 	service mariadb start
 
-	echo "---> Configuring MYSQL"
+	echo "Configuring MYSQL"
 	mariadb -u root < /var/lib/mysql/config_db.sql
 
-	echo "---> Changing root password"
+	echo "Changing root password"
 	mariadb -u root < /var/lib/mysql/change_root.sql
 
-	echo "---> Shuting down the database"
+	echo "Shutting down the database"
 	mariadb-admin --user=root --password=$SQL_PASSWORD shutdown
 
-	echo "---> Removing temporary files"
+	echo "Removing temporary files"
 	rm /var/lib/mysql/change_root.sql /var/lib/mysql/config_db.sql
 
 	touch /var/lib/mysql/first_config_done
 fi
 
-echo "---> Launching mysql service"
+unset DOMAIN_NAME SQL_DATABASE SQL_USER SQL_PASSWORD SQL_ROOT SQL_HOST SITE_TITLE ADMIN_USER ADMIN_PASSWORD ADMIN_EMAIL USER1_LOGIN USER1_MAIL USER1_PASS
+
+echo "Launching mysql service"
 exec mysqld_safe
--- inception/srcs/requirements/wordpress/tools/config_wp.sh
+++ inception/srcs/requirements/wordpress/tools/config_wp.sh
@@ -2,15 +2,17 @@
 
 if [ -f "/var/www/html/wp-config.php" ]
 then
-	echo "wordpress is already set !"
+	echo "Wordpress already configured"
 else
-	echo "I will set wp"
+	echo "Configuring wordpress"
 	./wp-cli.phar config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/html'
 	chmod 777 /var/www/html/wp-config.php
 	chown -R root:root /var/www/html
 	./wp-cli.phar core install --allow-root --url=$DOMAIN_NAME --title="$SITE_TITLE" --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/html'
 	./wp-cli.phar user create $USER1_LOGIN $USER1_MAIL --allow-root --role=author --user_pass=$USER1_PASS --path='/var/www/html'
-	echo "Wordpress script run!"
+	echo "Wordpress configured successfully"
 fi
+
+unset DOMAIN_NAME SQL_DATABASE SQL_USER SQL_PASSWORD SQL_ROOT SQL_HOST SITE_TITLE ADMIN_USER ADMIN_PASSWORD ADMIN_EMAIL USER1_LOGIN USER1_MAIL USER1_PASS
 
 exec /usr/sbin/php-fpm7.4 --nodaemonize
